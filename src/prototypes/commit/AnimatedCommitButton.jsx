34df4fcb5bdacedd732f5fdee9e18f22f4ab0b85
Victoria.Miroshnichenko <victoria.miroshnichenko@jetbrains.com>
2026-07-21 12:41:32 +0200

    Replace checkmark SVG with external image in AnimatedCommitButton components.

--- src/prototypes/commit/AnimatedCommitButton.jsx
+++ src/prototypes/commit/AnimatedCommitButton.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import { Loader } from '@jetbrains/int-ui-kit';
+import checkmrk2px from '../../../img/checkmrk2px.svg';
 import './CommitButtonDemo.css';
 
 const LOADING_DURATION_MS = 3000;
@@ -7,9 +8,7 @@ const LOADING_DURATION_MS = 3000;
 export function CommitedButton({ onClick }) {
   return (
     <button className="commit-btn-animated commit-btn-animated--commited" onClick={onClick}>
-      <svg width="16" height="16" viewBox="0 0 16 16" fill="none" aria-hidden="true" className="commit-btn-animated__icon">
-        <path d="M4 8.5L7 11.5L12.5 5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-      </svg>
+      <img src={checkmrk2px} width="16" height="16" aria-hidden="true" className="commit-btn-animated__icon" />
     </button>
   );
 }
